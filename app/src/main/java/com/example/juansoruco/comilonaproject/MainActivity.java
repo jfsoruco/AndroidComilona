3a0d884714217bd37bec11511ade5021eab508eb
jfsoruco <jfsoruco@gmail.com>
2015-09-04 10:10:43 -0400

    Activity para menus, boton de acceso y visualizacion de campos2

--- app/src/main/java/com/example/juansoruco/comilonaproject/MainActivity.java
+++ app/src/main/java/com/example/juansoruco/comilonaproject/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.juansoruco.comilonaproject;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.juansoruco.comilonaproject.employee.Employee;
@@ -21,6 +24,23 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Definicion del boton para llenar el menu del dia
+        final Button w_o_menu_button = (Button) findViewById(R.id.w_o_menus_button);
+        w_o_menu_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Recuperar el id de la orden semanal para pasarla a la pantalla
+                // de llenado del menu
+                int weekly_order_id = 1;
+
+                // TODO: invocar a pantalla secundaria para llenado del menu
+                Intent w_o_menu_intent = new Intent(getBaseContext(), WeeklyOrderActivity.class);
+                w_o_menu_intent.putExtra("WEEKLY_ORDER_ID", weekly_order_id);
+                startActivity(w_o_menu_intent);
+
+            }
+        });
 
         employeesList = getResources().getStringArray(R.array.employee_names);
         ArrayList<Employee> employees = new ArrayList<Employee>();
--- app/src/main/java/com/example/juansoruco/comilonaproject/WeeklyOrderActivity.java
+++ app/src/main/java/com/example/juansoruco/comilonaproject/WeeklyOrderActivity.java
@@ -2,8 +2,17 @@ package com.example.juansoruco.comilonaproject;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.juansoruco.comilonaproject.weeklyOrder.WeeklyOrder;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class WeeklyOrderActivity extends ActionBarActivity {
@@ -12,6 +21,34 @@ public class WeeklyOrderActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weekly_order);
+
+        int weekly_order_id = this.getIntent().getIntExtra("WEEKLY_ORDER_ID", 0);
+
+        TextView textView = (TextView)  findViewById(R.id.w_o_id);
+        textView.setText(String.valueOf(weekly_order_id));
+        Log.d("Id desde principal", Integer.toString(weekly_order_id));
+        String defaultMenu = "Llenar Comilona";
+        DateFormat df = new SimpleDateFormat("yyyy-mm-dd");
+
+        // TODO recuperar el registro del weekly order y mostrar formulario para llenar/editar menu
+        WeeklyOrder weeklyOrder = new WeeklyOrder(weekly_order_id, new Date(), 1, defaultMenu, defaultMenu, defaultMenu);
+        weeklyOrder.setGroupFullname("Grupo Independiente de Comilona");
+        weeklyOrder.setResponsibleFullname("Este eres tu");
+
+        TextView dateView = (TextView) findViewById(R.id.w_o_date);
+        TextView groupView = (TextView) findViewById(R.id.w_o_group);
+        TextView responsibleView = (TextView) findViewById(R.id.w_o_responsible);
+
+        EditText menu1View = (EditText) findViewById(R.id.w_o_menu1);
+        EditText menu2View = (EditText) findViewById(R.id.w_o_menu2);
+        EditText menu3View = (EditText) findViewById(R.id.w_o_menu3);
+
+        dateView.setText(df.format(weeklyOrder.getDate()));
+        groupView.setText(weeklyOrder.getGroupFullname());
+        responsibleView.setText(weeklyOrder.getResponsibleFullname());
+
+        // TODO botones de guardado y de completado
+
     }
 
     @Override
